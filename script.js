36364bd31d889eef68bf185fe8faec28e7623a08
CAS <78571139+cas-14@users.noreply.github.com>
2022-05-11 12:29:41 -0400

    fix random font

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 function randomFont() {
     var fonts = ["Arial", "Georgia", "Brush Script MT", "Trebuchet MS", "Times New Roman", "Courier New"];
-    document.getElementById("words").style.fontFamily = fonts[Math.floor((fonts.length) * Math.random())];
+    document.querySelector(":root").style.setProperty("--text-font", fonts[Math.floor((fonts.length) * Math.random())]);
 }

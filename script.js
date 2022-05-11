7cab925004a6ba12b51300f7242f78abe4903f7b
CAS <78571139+cas-14@users.noreply.github.com>
2022-05-11 16:26:44 -0400

    add the secret bernice code and optimize font code

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
+var fonts = ["Arial", "Georgia", "Brush Script MT", "Trebuchet MS", "Times New Roman", "Courier New"];
+
 function randomFont() {
-    var fonts = ["Arial", "Georgia", "Brush Script MT", "Trebuchet MS", "Times New Roman", "Courier New"];
-    document.querySelector(":root").style.setProperty("--text-font", fonts[Math.floor((fonts.length) * Math.random())]);
+    let font = fonts[Math.floor((fonts.length) * Math.random())];
+    document.querySelector(":root").style.setProperty("--text-font", font);
+    console.log("DEBUG: Font now set to "+font);
+}
+
+const berniceCode = "h,i,Space,b,e,r,n,i,c,e,";
+var keysPressed = "";
+
+document.onkeydown = (e) => {
+    e = e || window.event;
+    keysPressed = keysPressed + e.key + ",";
+    console.log("DEBUG: Detected key "+e.key);
+
+    if (keysPressed.includes(konamiCode)) {
+        keysPressed = "";
+        document.getElementById("words").innerHTML = "hi you have found a secret on the website, i didnt really plan this out so all you get for now is this message, nice work finding it though";
+    }
 }

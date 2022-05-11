0eaf11f090adac4f4f5625a47744e020e6ba1d4a
CAS <78571139+cas-14@users.noreply.github.com>
2022-05-11 08:39:49 -0400

    add rando mfont

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 function randomFont() {
     var fonts = ["Arial", "Georgia", "Brush Script MT", "Trebuchet MS", "Times New Roman", "Courier New"];
-    document.getElementById("words").style.fontFamily = fonts[Math.floor((keys.length) * Math.random())];
+    document.getElementById("words").style.fontFamily = fonts[Math.floor((fonts.length) * Math.random())];
 }

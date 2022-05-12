90ada31f2b26dfc5e5075c9158d38553f058e244
CAS <78571139+cas-14@users.noreply.github.com>
2022-05-12 10:23:09 -0400

    bernice moment

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 var fonts = ["Arial", "Georgia", "Brush Script MT", "Trebuchet MS", "Times New Roman", "Courier New"];
 
-function randomFont() {
-    let font = fonts[Math.floor((fonts.length) * Math.random())];
-    document.querySelector(":root").style.setProperty("--text-font", font);
-    console.log("DEBUG: Font now set to "+font);
+function randomize() {
+    document.querySelector(":root").style.setProperty("--font", fonts[Math.floor((fonts.length) * Math.random())]);
+    document.querySelector(":root").style.setProperty("--color", "#" + Math.floor(Math.random()*16777215).toString(16))
 }
 
-const berniceCode = "h,i, ,b,e,r,n,i,c,e,";
+const hiBernice = "h,i, ,b,e,r,n,i,c,e,";
+const strikesAgain = "b,e,r,n,i,c,e,s,t,r,i,k,e,s,a,g,a,i,n,"
 var keysPressed = "";
 
 document.onkeydown = (e) => {
@@ -14,8 +14,11 @@ document.onkeydown = (e) => {
     keysPressed = keysPressed + e.key + ",";
     console.log("DEBUG: Detected key "+e.key);
 
-    if (keysPressed.includes(berniceCode)) {
+    if (keysPressed.includes(hiBernice)) {
         keysPressed = "";
         document.getElementById("words").innerHTML = "hi you have found a secret on the website, i didnt really plan this out so all you get for now is this message, nice work finding it though";
+    } else if (keysPressed.includes(strikesAgain)) {
+        keysPressed = "";
+        alert("bernice moment! (this is a placeholder alert)")
     }
 }
